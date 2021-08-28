eb8ea3ec181322d6011e4a9d3f5a407cebdba481
Abhinav251005 <abhinav25102005@gmail.com>
2021-08-28 15:54:58 +0530

    1.0

--- portfolio/projects/ProjectMarvel/js/loadMore.js
+++ portfolio/projects/ProjectMarvel/js/loadMore.js
@@ -1,16 +1,20 @@
-const button = document.querySelector(".loadMore")
-const hiddenClass = "hiddenCard"
+const hiddenClass = "hidden"
 const maxCards = 3
 const loadCards = 3
+
+const loadBtn = document.querySelector(".loadMore")
+const collapseBtn = document.querySelector(".collapse")
+collapseBtn.classList.add(hiddenClass)
 
 var cards = document.getElementsByClassName("card")
 cards = Array.from(cards)
 
-cards.forEach((element, index) => {
+const hideCards = () => {cards.forEach((element, index) => {
     if (index > maxCards-1) {
         element.classList.add(hiddenClass)
     }
-})
+})}
+hideCards()
 
 const loadMore = () => {
     var hiddenCards = document.getElementsByClassName(hiddenClass)
@@ -20,9 +24,15 @@ const loadMore = () => {
             element.classList.remove(hiddenClass)
         }
         if (document.querySelectorAll(`.${hiddenClass}`).length === 0) {
-            button.textContent = "No More Characters"
-            button.classList.add("buttonDisabled")
+            loadBtn.classList.add(hiddenClass)
+            collapseBtn.classList.remove(hiddenClass)
         }
     }
     )
+}
+
+const collapse = () => {
+    hideCards()
+    collapseBtn.classList.add(hiddenClass)
+    loadBtn.classList.remove(hiddenClass)
 }
